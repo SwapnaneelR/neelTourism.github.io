f33efa335298bfedbfe2920792f9ec488f66e24e
SwapnaneelR <148081369+SwapnaneelR@users.noreply.github.com>
2023-12-29 22:00:50 +0530

    Add files via upload

---
+++ index.js
@@ -0,0 +1,4 @@
+let packagelist=  document.querySelector("package-list");
+packagelist.addEventListener("onmouseover",()=>{
+    document.querySelector("package-list div").style.backgroundColor("pink");
+})
